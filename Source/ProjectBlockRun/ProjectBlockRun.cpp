96fe8f592476fd506b9e17deafc96ab09ce6bf5e
Moz <amazumda@purdue.edu>
2024-01-17 21:56:51 -0500

    Initial Commit
    Added the folders according to the gitignore github page

---
+++ Source/ProjectBlockRun/ProjectBlockRun.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ProjectBlockRun.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectBlockRun, "ProjectBlockRun" );
---
+++ Source/ProjectBlockRun/ProjectBlockRunGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectBlockRunGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTBLOCKRUN_API AProjectBlockRunGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
